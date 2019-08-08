d3a8f62a884e7c7c66f423129ede20655cd751e8
PhongNguyen <quangnguyen51291@gmail.com>
2019-08-08 00:40:32 -0600

    fix get est date from equip update log

--- app/Http/Controllers/ApiReportController.php
+++ app/Http/Controllers/ApiReportController.php
@@ -46,20 +46,18 @@ class ApiReportController extends Controller
             $object->equipment_status = $d->equipment_status;
 
             if($d->equipment_status === 'DM'){
-                try{
                 $logEntry = DB::table('equip_update_logs')
                         ->where('unit', '=', $d->unit)
                         ->latest()
                         ->first();
-                $object->est_date_of_repair = $logEntry->est_date_of_repair;
-                }catch(Exception $e){
-                    dd($logEntry);
-                }
+                        
+                if($logEntry !== null)
+                    $object->est_date_of_repair = $logEntry->est_date_of_repair;
+                else
+                    $object->est_date_of_repair = null;
             }
-
             array_push($returnData, $object);
         }
-
         return $returnData;
     }
 }
